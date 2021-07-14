e54e68fa06b407952236dc2ee394728b4a5b5d5e
ornellabnchr <ornebanchero@hotmail.com>
2021-07-14 20:43:58 -0300

    test +

--- tpLab2.cpp/clsCliente.cpp
+++ tpLab2.cpp/clsCliente.cpp
@@ -9,6 +9,7 @@ using namespace std;
 #include "clsCliente.h"
 #include "validations.h"
 #include "clsOperacion.h"
+#include "clsPersona.h"
 
 
 bool Cliente::getPidioCredito(){return pidioCredito;};
@@ -59,17 +60,14 @@ void Cliente::cargarEnArchivo(){
 }
 
 int Cliente::buscarPosEnDisco(const int dni){
-      FILE *p;
-      p=fopen("Clientes.dat","rb");
-      if (p==NULL) return -1;
-      int c=0,pos=-1;
-      while(fread(this,sizeof (Cliente),1,p)==1){
-         if(this->dni==dni){
+Cliente reg;
+      int c=0,pos=0;
+      while(leerDeDisco(pos++)){
+         if(reg.getDni()==dni){
                 pos=c;
          }
           c++;
       }
-      fclose(p);
       return pos;
 }
 
@@ -115,15 +113,12 @@ int Cliente::calcularCantComprasRealizadas(const int dni){
 
 void Cliente::mostrarAutosAsociados(const int dni){
    Operacion regOperacion;
-   FILE *p;
-   p=fopen("Operaciones.dat","rb");
-   if (p==NULL) return;
-   while(fread(&regOperacion,sizeof (Operacion),1,p)==1){
+   int pos=0;
+   while(leerDeDisco(pos++)){
         if(regOperacion.getDniCliente()==dni){
             cout<<endl<< "Patente: "<<regOperacion.getDominioVehiculo();
         }
    }
-   fclose(p);
 };
 
 
